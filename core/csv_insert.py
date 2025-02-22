5661d7f3058a7617ff044d0bb024859f589176c9
Sprinter05 <sprinterhr@gmail.com>
2025-02-22 18:39:43 +0100

    fixed csv insertion

--- core/csv_insert.py
+++ core/csv_insert.py
@@ -1,5 +1,5 @@
 from core.models import *
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 import ollama
 
 def insert_langs():
@@ -10,7 +10,7 @@ def insert_langs():
         response = ollama.embed(model="nomic-embed-text", input=fields[1])
         embedding = Embedding(embedding=response["embeddings"][0], text=fields[1])
         embedding.save()
-        lang = Language(name=fields[1], description=fields[2], embedding_id=embedding)
+        lang = Language(name=fields[1], description=fields[2], embed_id=embedding)
         lang.save()
 
     file.close()
@@ -23,19 +23,19 @@ def insert_libs():
         response = ollama.embed(model="nomic-embed-text", input=fields[2])
         embedding = Embedding(embedding=response["embeddings"][0], text=fields[2])
         embedding.save()
-        lib = Library(name=fields[2], description=fields[3], lang_id=fields[1] ,embedding_id=embedding)
+        lib = Library(name=fields[2], description=fields[3], lang_id=Language(id=int(fields[1])) ,embed_id=embedding)
         lib.save()
 
     file.close()
 
 def insert_users():
-    file = open("resources/users", "r")
+    file = open("resources/users.csv", "r")
 
     for line in file:
         fields = line.split(",")
-        user = get_user_model
-        user.objects.createuser(id=fields[0], name=fields[1], email="", password="")
-
+        user = User.objects.create_user(username=fields[1], email="", password="")
+        user.save()
+        
     file.close()
 
 def insert_user_lib():
@@ -45,7 +45,7 @@ def insert_user_lib():
         fields = line.split(";")
         lib_ids = fields[1].split(",")
         for id in lib_ids:
-            userlib = UserLib(u_id=fields[0], lib_id=int(id))
+            userlib = UserLib(u_id=User(id=int(fields[0])), lib_id=Library(id=int(id)))
             userlib.save()
 
 
@@ -56,8 +56,8 @@ def insert_user_lang():
         fields = line.split(";")
         lang_ids = fields[1].split(",")
         for id in lang_ids:
-            userlib = UserLang(u_id=fields[0], lang_id=int(id))
-            userlib.save()
+            userlang = UserLang(u_id=User(id=int(fields[0])), lang_id=Language(id=int(id)))
+            userlang.save()
 
 insert_langs()
 insert_libs()
