69eb4e0260e3ccb064677568eb9a45493cf84509
Sprinter05 <sprinterhr@gmail.com>
2025-02-22 20:26:35 +0100

    fixed admin with new tables

--- core/admin.py
+++ core/admin.py
@@ -3,15 +3,6 @@ from django.contrib.auth.admin import UserAdmin
 from core.models import *
 
 # Register your models here.
-<<<<<<< HEAD
-admin.site.register(Embedding)
-admin.site.register(Language)
-admin.site.register(Library)
-admin.site.register(UserLib)
-admin.site.register(UserLang)
-<<<<<<< HEAD
-admin.site.register(User, UserAdmin)
-=======
 class EmbeddingAdmin(admin.ModelAdmin):
     model = Embedding
     list_display = ['id', 'text']
@@ -27,23 +18,6 @@ class LibraryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'description', 'lang_id', 'embed_id']
     search_fields = ['name']
 
-
-class UserLibAdmin(admin.ModelAdmin):
-    model = UserLib
-    list_display = ['u_id', 'lib_id']
-    search_fields = ['u_id']
-
-class UserLangAdmin(admin.ModelAdmin):
-    model = UserLang
-    list_display = ['u_id', 'lang_id']
-    search_fields = ['u_id']
-
 admin.site.register(Embedding, EmbeddingAdmin)
 admin.site.register(Language, LanguageAdmin)
 admin.site.register(Library, LibraryAdmin)
-admin.site.register(UserLib, UserLibAdmin)
-admin.site.register(UserLang, UserLangAdmin)
->>>>>>> origin/django
-=======
-admin.site.register(User, UserAdmin)
->>>>>>> origin/newdb
