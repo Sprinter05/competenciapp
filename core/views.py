84d014f0cb7d531e4be116a11bc52bb8e239310d
Sprinter05 <sprinterhr@gmail.com>
2025-02-22 12:17:20 +0100

    tried to implement embeddings

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from pgvector.django import L2Distance
+from models import Embeddings
+from django.http import HttpResponse
 
 # Create your views here.
+def data(vector):
+    objs = Embeddings.objects.order_by(
+        L2Distance('embedding', vector))[:10]
+    return HttpResponse(objs)
