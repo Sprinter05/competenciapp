963ceb2bf85226a26c105e37c0bb98295c396839
Markel Mencia <markel.mnc@gmail.com>
2025-02-22 01:49:06 +0100

    new init sql

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,30 @@
 CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE IF NOT EXISTS embeddings (
-  id SERIAL PRIMARY KEY,
-  embedding vector,
+  id SERIAL,
+  embedding vector(4096),
   text text,
-  created_at timestamptz DEFAULT now()
+  created_at timestamptz DEFAULT now(),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS competence (
+  id SERIAL,
+  name text,
+  description text,
+  embedding_id SERIAL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(id) REFERENCES embeddings(id)
+);
+
+CREATE TABLE IF NOT EXISTS auth_user (
+  id SERIAL,
+  username text,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS user_competence (
+  id_u SERIAL REFERENCES auth_user(id),
+  id_c SERIAL REFERENCES competence(id),
+  PRIMARY KEY(id_u, id_c)
 );
