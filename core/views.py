95cdf4ac4f1c01d95e96ee19802d8ab76ced901d
Sprinter05 <sprinterhr@gmail.com>
2025-02-22 13:01:14 +0100

    http ollama

--- core/views.py
+++ core/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from pgvector.django import L2Distance
 from models import Embeddings
 from django.http import HttpResponse
+from ollama import Client
+
+client = Client(
+    host="http://localhost:11434",
+)
 
 # Create your views here.
 def index(request):
@@ -14,7 +19,16 @@ def profile(request):
 def search(request):
     return HttpResponse("Search page. Query: " + request.GET.get("q", ""))
 
-def data(vector):
+def data(request):
     objs = Embeddings.objects.order_by(
-        L2Distance('embedding', vector))[:10]
-    return HttpResponse(objs)
+        L2Distance('embedding', request.GET.get("q", "")))[:10]
+    return HttpResponse(objs.values_list("text"))
+
+def llama(request):
+    prompt = request.GET.get("prompt", "")
+    data = request.GET.get("data", "")
+    response = client.generate(
+        model='llama3.2', 
+        prompt=f"Given this {data}, return the most relevant profiles according to {prompt}."
+    )
+    return HttpResponse(response)
